76ee664c76bf729f81eae094a870432987a931f9
Leonie <leonie.bloecher@gmx.de>
2016-04-02 21:50:45 +0200

    Movatar additions

--- app/src/main/java/de/htw_berlin/movation/Preferences.java
+++ app/src/main/java/de/htw_berlin/movation/Preferences.java
@@ -1,7 +1,7 @@
 package de.htw_berlin.movation;
 
 import org.androidannotations.annotations.sharedpreferences.SharedPref;
-
+import org.androidannotations.annotations.sharedpreferences.DefaultInt;
 
 @SharedPref(SharedPref.Scope.APPLICATION_DEFAULT)
 public interface Preferences {
@@ -15,4 +15,29 @@ public interface Preferences {
     int maxPulse();
     int minPulse();
     int successfullGoals();
+
+    @DefaultInt(0)
+    int indexGender(); // 0-1 (0 = female, 1 = male)
+
+    @DefaultInt(0)
+    int indexFitness(); // 0-2 (unfit, normal, fit)
+
+    @DefaultInt(0)
+    int indexHairstyle();
+
+    @DefaultInt(0)
+    int indexHairColor();
+
+    @DefaultInt(0)
+    int indexExpression();
+
+    @DefaultInt(0)
+    int indexEyeColor();
+
+    @DefaultInt(0)
+    int indexTop();
+
+    @DefaultInt(0)
+    int indexBottom();
 }
+
