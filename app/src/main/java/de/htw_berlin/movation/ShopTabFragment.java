414612223e5138e3151437f0a8ba75bd22a03214
Andrej Beliakov <andbel@gmail.com>
2016-04-04 01:08:09 +0200

    fix tabhost init problem

--- app/src/main/java/de/htw_berlin/movation/ShopTabFragment.java
+++ app/src/main/java/de/htw_berlin/movation/ShopTabFragment.java
@@ -1,62 +1,36 @@
 package de.htw_berlin.movation;
 
-import android.app.Activity;
-
-import android.app.FragmentTransaction;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTabHost;
-import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TabHost;
 
-import org.androidannotations.annotations.AfterInject;
-import org.androidannotations.annotations.AfterViews;
-import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.EFragment;
-import org.androidannotations.annotations.ViewById;
 
 /**
  * Created by Telan on 03.04.2016.
  */
-@EFragment(R.layout.fragment_tabcontainer)
+@EFragment
 public class ShopTabFragment extends Fragment {
 
     private FragmentActivity myContext;
-
-    @ViewById
-    FragmentTabHost tabhost;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        //setContentView(R.layout.fragment_tabcontainer);
-
-    }
+    // not injected due to bug with android.R resources
+    FragmentTabHost mTabHost;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        tabhost.setup(getActivity(),   getActivity().getSupportFragmentManager() , android.R.id.tabcontent);
-        tabhost.addTab(tabhost.newTabSpec("movatar Items").setIndicator("Movatar Items"),
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View layout = inflater.inflate((R.layout.fragment_tabcontainer), null);
+        mTabHost = (FragmentTabHost) layout.findViewById(android.R.id.tabhost);
+        mTabHost.setup(getActivity(), getChildFragmentManager(), android.R.id.tabcontent);
+        mTabHost.addTab(mTabHost.newTabSpec("movatar Items").setIndicator("Movatar Items"),
                 ShopFragment.class, null);
-        tabhost.addTab(tabhost.newTabSpec("discounts").setIndicator("Discounts"),
+        mTabHost.addTab(mTabHost.newTabSpec("discounts").setIndicator("Discounts"),
                 ShopFragment.class, null);
 
-        return null;
+        return layout;
     }
-
-
-    @AfterViews
-    public void init()
-    {
-
-    }
-
 }
